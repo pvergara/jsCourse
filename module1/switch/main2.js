ccdbb27d8f1febe59b0f64864259a9d3c377d310
Pablo Cristóbal Vergara Castro <pablocristobal@gmail.com>
2015-10-25 23:59:12 +0100

    Lesson 1 Module1 (switch)   It seems that this exercises are not under the module2

---
+++ module1/switch/main2.js
@@ -0,0 +1,17 @@
+var x = 1;
+var message = "";
+switch (true){
+    case x > 0 && x <10:
+        message += "x > 0 && x <10";
+        //break;
+    case x > 0 && x < 20:
+        message += "x > 0 && x < 20";
+        break;
+    case x < 0 || x >20:
+        message = "x < 0 || x >20";
+        break;
+    default:
+        message = "No cases";
+        break;
+}
+console.log(message);
