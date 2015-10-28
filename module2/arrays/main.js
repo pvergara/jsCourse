db1e91daf19a560c49383e6426c986771ece7657
Pablo Cristóbal Vergara Castro <pablocristobal@gmail.com>
2015-10-28 23:45:14 +0100

    Lesson 1 Module 2   Arrays

---
+++ module2/arrays/main.js
@@ -0,0 +1,10 @@
+var bands = [];
+bands[0] = "Between the buried and me";
+bands[1] = "Ghost";
+bands[2] = "Manowar";
+bands[3] = "Dream theater";
+bands[4] = "Transatlantic";
+
+for(var index in bands) {
+    console.log("bands[{0}] = {1}".format(index,bands[index]));
+}
---
+++ module2/arrays/mainAnotherWayToInitialize.js
@@ -0,0 +1,11 @@
+var bands = [
+        "Between the buried and me",
+        "Ghost",
+        "Manowar",
+        "Dream theater",
+        "Transatlantic",
+    ];
+for(var index in bands) {
+    console.log("bands[{0}] = {1}".format(index,bands[index]));
+}
+
