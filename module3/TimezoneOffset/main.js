c035d1cead603c75702406dc52f1dfd745ab8019
Pablo Cristóbal Vergara Castro <pablocristobal@gmail.com>
2015-11-04 23:10:59 +0100

    Module 3   UTCDate   TimezoneOffset

---
+++ module3/TimezoneOffset/main.js
@@ -0,0 +1,2 @@
+var date = new Date();
+console.log("Actual timezone offset between local date and UTC date: {0}".format(date.getTimezoneOffset()));
---
+++ module3/UTCDates/main.js
@@ -0,0 +1,2 @@
+var fecha = new Date(2013,0,1,0,15);
+alert(fecha + "\n" + fecha.toUTCString());
