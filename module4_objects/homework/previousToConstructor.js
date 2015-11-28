f31cc27667165599943d871ceaf12947e408c1fb
Pablo Cristóbal Vergara Castro <pablocristobal@gmail.com>
2015-11-28 17:49:45 +0100

    Module 4 (objects)   * Homework     + previous to the second exercise

---
+++ module4_objects/homework/previousToConstructor.js
@@ -0,0 +1,31 @@
+function AClass(hello){
+    var _hello=hello;
+
+    AClass.prototype.getHelloFromProto = function(){
+        return _hello;
+    };
+
+    this.getHello = function (){
+        return _getHello();
+    };
+
+    function _getHello(){
+        return _hello;
+    }
+
+    var _getHelloFromVar = function (){
+        return _hello;
+    };
+}
+
+var object = new AClass("hello");
+var object2 = new AClass("Helloween");
+
+
+console.log("typeof object.getHelloFromProto: {0}".format(typeof object.getHelloFromProto));
+console.log("typeof object.getHello: {0}".format(typeof object.getHello));
+console.log("typeof object._getHello: {0}".format(typeof object._getHello));
+console.log("typeof object._getHelloFromVar: {0}".format(typeof object._getHelloFromVar));
+
+console.log("The return value of object.getHelloFromProto() is: {0}".format(object.getHelloFromProto()));
+console.log("The return value of object.getHello() is: {0}".format(object.getHello()));
