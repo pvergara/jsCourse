cfdb0cd7175bce75f86c63c474a7676019b652c8
Pablo Cristóbal Vergara Castro <pablocristobal@gmail.com>
2015-12-13 09:47:30 +0100

    Module 5 (DOM+BOM)

---
+++ module5_BOM/screen/main.js
@@ -0,0 +1,7 @@
+console.log("window.screen.width: {0}, window.screen.height: {1}, window.screen.availWidth: {2}, window.screen.availHeight: {3}, window.screen.colorDepth: {4}".format(
+    window.screen.width,
+    window.screen.height,
+    window.screen.availWidth,
+    window.screen.availHeight,
+    window.screen.colorDepth)
+);
